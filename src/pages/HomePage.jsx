6dfac32ffaa97aa3f900860987b644ba46cdc446
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-04-02 16:02:44 -0700

    Added QuickLinkCards and text content with styling to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 // import {useState, useEffect} from 'react';
 
+import QuickLinkCard from "../components/common/QuickLinkCard";
+
 // import '../App.css';
 
 
@@ -7,8 +9,16 @@ function HomePage () {
 
     return (
         <>
-            {/* <div className=''></div> */}
-            <h1>Home</h1>
+            <div className='genericTextBlock'>Click an option below to quick start a test! Or go to the "Basic Typing Tests" tab for more test options or to create a custom test.</div>
+            <hr className='sectionDividerTop'/>
+            
+            <span className='quickStartTestRow'>
+                <QuickLinkCard cardText={'500 WORD-COUNT BASED TEST'} linkTo={'/BasicTypingTests'} />
+                <QuickLinkCard cardText={'1 MINUTE TIMED TEST'} linkTo={'/BasicTypingTests'} />
+            </span>
+
+            <hr className='sectionDividerBottom'/>
+            <div className='genericTextBlock'>You can also sign in or make an account to experience all the features [insert site name] has to offer!</div>
         </>
     )
 }
