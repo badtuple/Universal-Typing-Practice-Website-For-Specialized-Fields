89dbd66e5d24d5e9787209ec10ae0953b9a41022
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-04-11 17:37:53 -0700

    Added components for all the different custom test options and their basic boilerplate

---
+++ src/components/custom-test-section/OptionInsertionPointStyle.jsx
@@ -0,0 +1,29 @@
+// import {useState, useEffect} from 'react';
+
+import '/src/App.css';
+
+
+function OptionInsertionPointStyle () {
+
+    // const [accordionSectionOpen, setAccordionSectionOpen] = useState(false)
+
+    // // checks if accordion component is clicked
+    // const updateAccordionStatus = function () {
+    //     if (accordionSectionOpen) {
+    //         setAccordionSectionOpen(false);
+    //     }
+    //     else {
+    //         setAccordionSectionOpen(true);
+    //     }
+    // }
+
+    return (
+        <>
+            <div className='customTestOptionsAccordion' onClick={updateAccordionStatus}>
+                <span></span>
+            </div>
+        </>
+    )
+}
+
+export default OptionInsertionPointStyle;
